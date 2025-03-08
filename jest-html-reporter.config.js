7d73161da789848a192fb1ee06035f853899fddf
david <tridavid222@gmail.com>
2025-03-09 04:43:27 +0530

    gg

---
+++ jest-html-reporter.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pageTitle: 'Code Coverage Report',
+  outputPath: './coverage-report.html',
+  includeFailureMsg: true,
+  includeConsoleLog: true,
+  sort: 'alphabetical',
+};
